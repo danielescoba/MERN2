ea188d49bd01eed524b3376069f12bd4a1f9689b
Daniel Escobar <68076771+danielescoba@users.noreply.github.com>
2020-10-26 21:23:50 -0400

    'update'

---
+++ Assignments/propitup/src/Components/PersonCard.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class PersonCard extends Component {
+    render() {
+        const{firstName, lastName, age, hairColor} = this.props;
+        return (
+            <div>
+                <h2>{firstName}, {lastName}</h2>
+        <p>Age: {age}</p><br></br>
+        <p>Hair Color: {hairColor}</p>
+            </div>
+        )
+    }
+}
+
